32209ca1f016c66a8c4aad2f54fcbec0750f21e4
Guillaume COLOMBEL <gui.colombel76@gmail.com>
2020-12-19 21:00:08 +0100

    Mise en place du php

---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Choistory</title>
+</head>
+
+<body>
+
+<header>
+    <div class="top-header"></div>
+    <div class="menu_header">
+        <div class="option_menu"><a href="index.html">Accueil</a></div>
+        <div class="option_menu"><a href="">Mes histoires</a></div>
+        <div class="option_menu"><a href="">Qui suis-je ?</a></div>
+        <div class="option_menu"><a href="">Contact</a></div>
+    </div>
+    <div class="bottom-header"></div>
+</header>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+include('inc/header.php'); ?>
+
+<div class="home_page">
+
+    <!--dust particel-->
+    <div>
+        <div class="starsec"></div>
+        <div class="starthird"></div>
+        <div class="starfourth"></div>
+        <div class="starfifth"></div>
+    </div>
+    <!--Dust particle end--->
+
+    <div class="lamp__wrap">
+        <div class="lamp">
+            <div class="cable"></div>
+            <div class="cover"></div>
+            <div class="in-cover">
+                <div class="bulb"></div>
+            </div>
+            <div class="light"></div>
+        </div>
+    </div>
+    <!-- END Lamp -->
+    <section class="error">
+        <!-- Content -->
+        <div class="error__content">
+            <div class="error__message message">
+                <h1 class="message__title">Choistory</h1>
+            </div>
+        </div>
+        <!-- END Content -->
+    </section>
+</div>
+
+<?php include('inc/footer.php'); ?>
