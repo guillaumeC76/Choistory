873cfd3797e8d9db0166287d979740d46654b4b5
Guillaume COLOMBEL <gui.colombel76@gmail.com>
2020-12-19 21:57:20 +0100

    page histoire créer

--- histoires.php
+++ histoires.php
@@ -9,7 +9,7 @@
             <div class="texte_crispaux">
                 <h2>Crispaux</h2>
                 <br>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias voluptate suscipit nam molestiae reprehenderit possimus exercitationem nisi quaerat dolorum nostrum! Praesentium laborum corrupti officiis itaque ratione nemo porro voluptas veniam.</p>
+                <p>Beaucoup de jeunes enfants rêvent de devenir agents secrets lorsqu'ils seront plus grand. Aujourd'hui, Peko 31 ans, agent secret et surtout bientot papa. Réussirez vous à garder sa femme en vie avant le terrible attentat sur Crispaux ? <br>A vous de jouer ➔</p>
             </div>
         </div>
     </a>
