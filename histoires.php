b67ec2b15200df2cbf945c0c584f89d13092b0e7
guillaumeC76 <57393436+guillaumeC76@users.noreply.github.com>
2020-12-20 18:20:21 +0100

    commit histoire

--- histoires.php
+++ histoires.php
@@ -24,21 +24,21 @@ include('inc/header.php'); ?>
 
     <div class="crispaux">      
         <div class="texte_crispaux">
-            <h2>Crispaux</h2>
+            <h2>L'arbre Oscultant</h2>
             <br>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias voluptate suscipit nam molestiae reprehenderit possimus exercitationem nisi quaerat dolorum nostrum! Praesentium laborum corrupti officiis itaque ratione nemo porro voluptas veniam.</p>
         </div>
         <div class="image_crispaux">
-            <img src="assets/img/ville-moderne.jpg" alt="Illustration de crispaux">
+            <img src="assets/img/arbre.jpg" alt="Illustration de crispaux">
         </div>
     </div>
 
     <div class="crispaux">
         <div class="image_crispaux">
-            <img src="assets/img/ville-moderne.jpg" alt="Illustration de crispaux">
+            <img src="assets/img/ninja.jpg" alt="Illustration de crispaux">
         </div>
         <div class="texte_crispaux">
-            <h2>Crispaux</h2>
+            <h2>Ninja vengeur</h2>
             <br>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias voluptate suscipit nam molestiae reprehenderit possimus exercitationem nisi quaerat dolorum nostrum! Praesentium laborum corrupti officiis itaque ratione nemo porro voluptas veniam.</p>
         </div>
