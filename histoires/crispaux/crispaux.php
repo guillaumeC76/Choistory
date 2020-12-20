c803c2ed4fe2eabd3bf840e5cf38c53b0fe99a00
Guillaume COLOMBEL <gui.colombel76@gmail.com>
2020-12-20 01:02:30 +0100

    commit fin de journée

--- histoires/crispaux/crispaux.php
+++ histoires/crispaux/crispaux.php
@@ -4,7 +4,7 @@ include('incHistoire/functionHistoire.php');
 include('incHistoire/headerHistoire.php'); ?>
 
 <div class="histoire_page">
-    
+     
 </div>
 
 <?php include('incHistoire/footerHistoire.php'); ?>
